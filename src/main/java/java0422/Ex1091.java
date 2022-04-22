bf9e2928b8ea40fd103f767f4b7c966b70307a4f
hyegyeong <hyegyeong25@gmail.com>
2022-04-22 12:56:59 +0900

    22.04.22 - code up

--- src/main/java/java0422/Ex1091.java
+++ src/main/java/java0422/Ex1091.java
@@ -2,30 +2,32 @@ package java0422;
 
 import java.util.Scanner;
 
-public class Ex1090 {
-    //codeup 등비수열
+public class Ex1091 {
+    // 수열 3번째 문제
+    // 시작 값(a), 곱할 값(m), 더할 값(d), 몇 번째 인지를 나타내는 정수(n)
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        int r = scan.nextInt();
+        int m = scan.nextInt();
+        int d = scan.nextInt();
         int n = scan.nextInt();
         int i = 1;
         long result = a;
 
-        //while
-        while(true) {
+        //while 풀이
+        while(true){
             if(i == n) break;
             i++;
-            result = result * r;
+            result = (result*m)+d;
         }
         System.out.println(result);
 
         //초기화
         i = 1; result = a;
 
-        //for
+        //for 풀이
         for(i = 1; i < n; i++){
-            result = result * r;
+            result = (result*m)+d;
         }
         System.out.println(result);
     }
